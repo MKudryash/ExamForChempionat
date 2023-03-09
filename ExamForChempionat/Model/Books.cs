db142b5a70e774daa1996279227a84d3d1f75190
MKudryash <Jukudryash@gmail.com>
2023-03-09 12:42:40 +0300

    Добавьте файлы проекта.

---
+++ ExamForChempionat/Model/Books.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamForChempionat.Model
+{
+    public class Bookss
+    {
+        public List<Book> json { get; set; }
+
+    }
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Cost { get; set; }
+        public int StockAvailability { get; set; }
+        public int AvailabilityInTheStore { get; set; }
+        public string Description { get; set; }
+        public string Rewiews { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ ExamForChempionat/Model/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ExamForChempionat.Model
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public async Task<Bookss> GetApi()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(@"https://iis.ngknn.ru/NGKNN/МамшеваЮС/Экзамен/api/Books");
+            var json = response.Content.ReadAsStringAsync().Result;
+            allBooks = JsonConvert.DeserializeObject<List<Book>>(json);
+
+            PropertyChanged(this, new PropertyChangedEventArgs("GetBooks"));
+            return booksJson;
+        }
+        Bookss booksJson;
+        List<Book> allBooks { get; set; } = new List<Book>();
+
+        public List<Book> GetBooks  
+        {
+            get
+            {
+                    return allBooks;
+            }
+        }
+
+    }
+}
